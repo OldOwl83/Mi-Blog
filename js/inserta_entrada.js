a94203ab73dc2b990d0b3e2cebcf1c3c6bbead97
Mauro <maurodonnamora@gmail.com>
2021-12-09 11:13:04 -0300

    Carga de scripts. Entrada BlackJack

--- js/inserta_entrada.js
+++ js/inserta_entrada.js
@@ -7,15 +7,29 @@ function Inserta_entrada(arch_entrada)
     {
         cont_princ.innerHTML = "";
 
-                // if(!this.responseXML.includes("Error 404"))
-            cont_princ.appendChild(this.responseXML.getElementsByClassName("cont_publicaciones")[0]);
+        if(!this.responseText.includes("Error 404"))
+        {
+            cont_princ.insertAdjacentHTML("afterBegin", this.responseText);
 
-        // else
-        //     cont_princ.insertAdjacentHTML("afterBegin", "<h4>Página en construcción (" + encodeURI(arch_entrada) + ")</h4><p>Disculpe las molestias. Ya puede visitar las siguientes entradas: <ul><li>Sobre este blog</li><li>Contacto</li><li>Batalla Naval</li><li>BuscaBombas</li><li>Calculín</li><li>BlackJack</li>");
+            switch(arch_entrada)
+            {
+                case "publicaciones/Buscabombas.htm":
+                    let script = document.createElement("script");
+                    script.type = "text/javascript";
+                    script.src = "sitios/BuscaBombas/clase_Buscabombas.js";
+                    document.body.appendChild(script);
+
+                    script = document.createElement("script");
+                    script.type = "text/javascript";
+                    script.src = "js/insertar_buscabombas.js";
+                    document.body.appendChild(script);
+
+                    break;
+            }
+        }else
+            cont_princ.insertAdjacentHTML("afterBegin", "<h4>Página en construcción (" + encodeURI(arch_entrada) + ")</h4><p>Disculpe las molestias. Ya puede visitar las siguientes entradas: <ul><li>Sobre este blog</li><li>Contacto</li><li>Batalla Naval</li><li>BuscaBombas</li><li>Calculín</li><li>BlackJack</li>");
     }
 
     pedido.open("GET", arch_entrada, true);
-    pedido.responseType = "document";
-    pedido.overrideMimeType("text/html");
     pedido.send();
 }
--- js/pruebas_offline.js
+++
@@ -1,1 +0,0 @@
-document.getElementById("contenedor_princ").innerHTML = '';
